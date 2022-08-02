bb58427906d919e0db31e61a38079c421e10d442
dr <1561500402@qq.com>
2022-08-02 15:14:31 +0800

    登录框功能完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,33 +9,57 @@ $("#link_login").on("click", function () {
 });
 
 const form = layui.form;
-const baseUrl = 'http://www.liulongbin.top:3007'
+const baseUrl = "http://www.liulongbin.top:3007";
+const layer = layui.layer;
+// layer.msg('hell')
 
 form.verify({
-    repass:value => {
-        const paw = $('.reg-box [name = password').val()
-        if(paw !==value) return '两次密码不一致'
-    },
+  repass: (value) => {
+    const paw = $(".reg-box [name = password").val();
+    if (paw !== value) return "两次密码不一致";
+  },
   //我们既支持上述函数式的方式，也支持下述数组的形式
   //数组的两个值分别代表：[正则匹配、匹配不符时的提示文字]
-  pass: [
-    /^[\S]{6,12}$/
-    ,'密码必须6到12位，且不能出现空格'
-  ] 
+  pass: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
 });
 
-$('#form_reg').on('submit',function(e){
-    e.preventDefault()
-    // console.log(1);
-    // const data = $(this).serialize()
-    const data ={
-        username:$('.reg-box [name = username').val(),
-        password:$('.reg-box [name = password').val()
-    }
-    $.ajax({
-        type:'POST',
-        url:baseUrl+'/api/reguser',
-        data,
-        sucess:res => console.log(res),
-    })
-})
+$("#form_reg").on("submit", function (e) {
+  e.preventDefault();
+  // console.log(1);
+  // const data = $(this).serialize()
+  const data = {
+    username: $(".reg-box [name = username").val(),
+    password: $(".reg-box [name = password").val(),
+  };
+  $.ajax({
+    type: "POST",
+    url: baseUrl + "/api/reguser",
+    data,
+    success: (res) => {
+      console.log(res);
+      const { message, status } = res;
+      if (status !== 0) return layer.msg(message);
+      $("#link_login").click();
+    },
+  });
+});
+
+$("#form_login").on("submit", function (e) {
+  e.preventDefault();
+
+  const data = $(this).serialize()
+
+  $.ajax({
+    type: "POST",
+    url: baseUrl + "/api/login",
+    data,
+    //    return console.log(res);
+    success: (res) => {
+      console.log(res);
+      const { message, status ,token } = res;
+      if (status !== 0) return layer.msg(message);
+      localStorage.setItem('token',token)
+      location.href = '/index.html'
+    },
+  });
+});
