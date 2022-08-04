63c275cdc2a15fdda8c2825c1e39f0fc76868f23
dr <1561500402@qq.com>
2022-08-04 11:21:52 +0800

    index基本内容完成

--- assets/js/baesApi.js
+++ assets/js/baesApi.js
@@ -3,6 +3,19 @@ const baseUrl = "http://www.liulongbin.top:3007";
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter((option) => {
-    // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-    option.url = baseUrl + option.url;
-  });
+  // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+  if (option.url.includes("/my")) {
+    option.headers = {
+      Authorization: localStorage.getItem("token"),
+    };
+  }
+  // 不论成功还是失败，最终都会调用 complete 回调函数
+  option.complete = res => {
+    if(res.responseJSON.status ===1 && res.responseJSON.message === "身份认证失败！"){
+      //强制清空 token
+      localStorage.removeItem('token')
+      location.herf = '/login.html'
+    }
+  }
+    (option.url = baseUrl + option.url);
+});
